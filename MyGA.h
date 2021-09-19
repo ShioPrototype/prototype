8d6cb2f2b7d829315f0a8cc1ca0f18e719233135
张昊天 <zhanghaotian0411@163.com>
2021-09-19 20:31:47 +0800

    做过一些测试，决定了程序的大致结构

---
+++ MyGA.h
@@ -0,0 +1,15 @@
+#ifndef MYGA_H_INCLUDED
+#define MYGA_H_INCLUDED
+
+bool NewGroupGeneration(int *head,int *limits,int n){
+/*用于产生新种群，即随机地依照范围limits产生n维
+相当于函数自变量的列向量，并输入到head数组中
+返回值代表是否产生成功，limits是一个二维数组，第一行和第二行
+分别代表每一个变量的上界与下界，下界不小于上界会导致产生失败。
+*/
+/*这个函数是用作1维向量（即单变量函数），另有一用作多维向量的
+版本使用*/
+
+}
+
+#endif // MYGA_H_INCLUDED
---
+++ testmain.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"MyGA.h"
+using namespace std;
+int main(){
+    int x=1;
+    cout<<testhead(x)<<endl;
+    return 0;
+}
